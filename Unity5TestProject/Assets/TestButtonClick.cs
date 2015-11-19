93714d720fe0f409781fa9596f12110f25ccf4d4
Apple <apple@Apple-no-MacBook-Air.local>
2015-11-19 17:54:07 +0900

    first version of unity5push project

---
+++ Unity5TestProject/Assets/TestButtonClick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestButtonClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Button button = (Button)this.GetComponent<Button> ();
+		button.onClick.AddListener (() => {
+			Debug.Log("Clicked");
+			Application.LoadLevel("Base");
+		});
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+//	public void ClickTest() {
+//		Debug.Log ("Clicked");
+//	}
+}
